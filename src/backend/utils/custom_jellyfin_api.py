17e7bd752a159c1d093b4427ac56350dae784a13
mah-thingies <155659937+mah-thingies@users.noreply.github.com>
2024-07-22 21:15:54 +0200

    Create custom_jellyfin_api.py

---
+++ src/backend/utils/custom_jellyfin_api.py
@@ -0,0 +1,8 @@
+import requests
+from utils.custom_emby_api import EmbyAPI
+
+
+class JellyfinAPI(EmbyAPI):
+    def get_users(self):
+        url = self._url_builder("/Users")
+        return self._get_request(url)
